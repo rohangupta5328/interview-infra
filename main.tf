ed83d29f5b753cb1c069e58dbc188be880ce9178
Rohan Gupta <rohan.gupta@harness.io>
2019-11-08 15:41:44 -0800

    uploade

--- main.tf
+++ main.tf
@@ -1,44 +1,42 @@
 provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key
-    region = var.aws_region
-    
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.aws_region
 }
 
 terraform {
-    backend "s3" {
+  backend "s3" {
     bucket = "rohan-tf-remote-state"
-    key = "terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
-    }
+  }
 }
 
 resource "aws_s3_bucket" "rohan-bb-terra" {
-    bucket = "${var.s3_bucket_name}"
-    acl = "private"
-    region = "${var.aws_region}"
+  bucket = var.s3_bucket_name
+  acl    = "private"
+  region = var.aws_region
 
-    versioning {
-        enabled = true 
-    }
+  versioning {
+    enabled = true
+  }
 
-    tags = {
-        Name = "${var.s3_bucket_name}"
-        Environment = "${var.tag_env}"
-    }
+  tags = {
+    Name        = var.s3_bucket_name
+    Environment = var.tag_env
+  }
 }
 
 resource "aws_instance" "ec2" {
-    ami = "${var.ami_id}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.aws_key}"
-    count = 3
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.aws_key
+  count         = 1
 
-    tags = {
-        Name = "${var.name}"
-        Environment = "${var.tag_env}"
-        Department = "harnessbb"
-    }
-
-
+  tags = {
+    Name        = var.name
+    Environment = var.tag_env
+    Department  = "harnessbb"
+  }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,43 +1,47 @@
+variable "access_key" {
+}
 
-variable "access_key" {}
-variable "secret_key" {}
+variable "secret_key" {
+}
 
 variable "aws_region" {
-    description = "AWS region"
-    default = "us-east-1"
+  description = "AWS region"
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
-    description = "ID of the AMI to provision. Default is AWS Base Image"
-    default = "ami-6d1c2007"
+  description = "ID of the AMI to provision. Default is AWS Base Image"
+  default     = "ami-6d1c2007"
 }
 
 variable "instance_type" {
-    description = "type of EC2 instance to provision."
-    default = "t2.micro"
+  description = "type of EC2 instance to provision."
+  default     = "t2.micro"
 }
 
 variable "name" {
-    description = "name to pass to Name tag"
-    default = "rohantf"
+  description = "name to pass to Name tag"
+  default     = "rohantf"
 }
 
-variable "aws_key" {}
+variable "aws_key" {
+}
 
 variable "s3_bucket_name" {
-  type        = "string"
+  type        = string
   description = "Name of the S3 bucket to be created used to store the remote state file"
   default     = "rohan-bb-terra"
-  }
+}
 
 variable "tag_env" {
-  type        = "string"
+  type        = string
   description = "Tag for Environment Name"
   default     = "dev"
-  }
+}
 
-  variable "department" {
-      type = "string"
-      description = "tag for the department"
-      default = "harnessbb"
-  }
+variable "department" {
+  type        = string
+  description = "tag for the department"
+  default     = "harnessbb"
+}
+
