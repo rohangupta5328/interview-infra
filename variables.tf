a93a87da2e8a3acbd47cad2570a24745143c976d
Rohan Gupta <rohan.gupta@harness.io>
2019-11-08 13:16:17 -0800

    initial commit

---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "aws_region" {
+  description = "Region where Cloud Formation is created"
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the AWS Minikube cluster - will be used to name all created resources"
+}
+
+variable "tags" {
+  description = "Tags used for the AWS resources created by this template"
+  type        = map(string)
+}
+
+variable "addons" {
+  description = "list of YAML files with Kubernetes addons which should be installed"
+  type        = list(string)
+}
+
+variable "aws_instance_type" {
+  description = "Type of instance"
+  default     = "t2.large"
+}
+
+variable "aws_subnet_id" {
+  description = "The subnet-id to be used for the instance"
+}
+
+variable "ssh_public_key" {
+  description = "Path to the pulic part of SSH key which should be used for the instance"
+  default     = "adminRohan"
+}
+
+variable "hosted_zone" {
+  description = "Hosted zone to be used for the alias"
+}
+
+variable "hosted_zone_private" {
+  description = "Is the hosted zone public or private"
+  default     = false
+}
+
+variable "ami_image_id" {
+  description = "ID of the AMI image which should be used. If empty, the latest CentOS 7 image will be used. See README.md for AMI image requirements."
+  default     = ""
+}
