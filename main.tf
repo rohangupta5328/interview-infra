564e8745da9d184f65b636a11686fbe6adb6d721
Rohan Gupta <rohan.gupta@harness.io>
2019-11-08 15:47:36 -0800

    new

--- main.tf
+++ main.tf
@@ -12,20 +12,6 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "rohan-bb-terra" {
-  bucket = var.s3_bucket_name
-  acl    = "private"
-  region = var.aws_region
-
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = var.s3_bucket_name
-    Environment = var.tag_env
-  }
-}
 
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
